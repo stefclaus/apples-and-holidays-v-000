dffde421b4dba2e2738358f4366b1ec716072b63
Stef Claus <stef.claus@gmail.com>
2018-09-23 01:48:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,6 @@ def all_holidays_with_bbq(holiday_hash)
         if supply.include? "BBQ" 
           answer << holiday_name.to_s
           answer.join(" ")
-        end 
     end #ends second iteration 
   end # ends first iteration 
   end #ends 
